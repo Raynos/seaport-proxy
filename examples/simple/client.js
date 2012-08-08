7779d52acc41b0ccf5fd3ca6ee9f5c59b14f5b3f
Raynos <raynos2@gmail.com>
2012-08-08 10:15:20 -0700

    dead files

--- examples/simple/client.js
+++
@@ -1,8 +0,0 @@
-var boot = require("boot")
-    , seaport = require("../..")
-    , ports = seaport(boot("/boot"))
-
-var stream = ports.get("magic@1.2.x")
-stream.on("data", console.log.bind(console, "[BROWSER]"))
-
-stream.write("hello from browser!")
