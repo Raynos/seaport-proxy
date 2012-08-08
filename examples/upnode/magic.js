02499eadf73966d0120c35c68ee454aa494df80a
Raynos <raynos2@gmail.com>
2012-08-08 00:53:28 -0700

    refactored upnode example

--- examples/upnode/magic.js
+++ examples/upnode/magic.js
@@ -1,20 +1,15 @@
-var seaport = require("seaport")
+var seaport = require("seaport-stream")
     , upnode = require("upnode")
-    , net = require("net")
 
 var ports = seaport.connect("localhost", 9093)
 
 ports.service("magic@1.2.3", createMagic)
 
-function createMagic(port, done) {
-    var server = net.createServer(function (stream) {
-        var up = upnode(function (client, conn) {
-            this.time = function (cb) {
-                cb(new Date().toString())
-            }
-        })
-        up.pipe(stream).pipe(up)
+function createMagic(stream) {
+    var up = upnode(function (client, conn) {
+        this.time = function (cb) {
+            cb(new Date().toString())
+        }
     })
-    server.listen(port, done)
-    console.log("magic service hooked on port", port)
+    up.pipe(stream).pipe(up)
 }
